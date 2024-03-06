2d51b092ad64d54f2fcbe482fde6832c43bace67
Andre <andre.ats.profissional@gmail.com>
2024-03-06 00:32:05 -0300

    mudanca database

---
+++ Domain/Migrations/20240301061802_Migration01.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VinilProjeto.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    senha = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsuarioComprador",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    senha = table.Column<string>(type: "text", nullable: false),
+                    telefone_codigo = table.Column<string>(type: "text", nullable: false),
+                    telefone_ddd = table.Column<string>(type: "text", nullable: false),
+                    telefone_numero = table.Column<string>(type: "text", nullable: false),
+                    endereco_cep = table.Column<string>(type: "text", nullable: false),
+                    endereco_logradouro = table.Column<string>(type: "text", nullable: false),
+                    endereco_numero = table.Column<string>(type: "text", nullable: false),
+                    endereco_complemento = table.Column<string>(type: "text", nullable: false),
+                    endereco_referencia = table.Column<string>(type: "text", nullable: false),
+                    endereco_bairro = table.Column<string>(type: "text", nullable: false),
+                    endereco_cidade = table.Column<string>(type: "text", nullable: false),
+                    endereco_estado = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuarioComprador", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vinil",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    nomeVinil = table.Column<string>(type: "text", nullable: false),
+                    descricaoVinil = table.Column<string>(type: "text", nullable: false),
+                    precoVinil = table.Column<string>(type: "text", nullable: false),
+                    quantiaVinil = table.Column<string>(type: "text", nullable: false),
+                    EstiloMusical = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vinil", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "UsuarioComprador");
+
+            migrationBuilder.DropTable(
+                name: "Vinil");
+        }
+    }
+}
